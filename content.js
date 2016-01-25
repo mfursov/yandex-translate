79e3c24d52fee1efa2f6e36ab7d9ccc1f2e5bcbd
Mikhail Fursov <fursovm@amazon.com>
2016-01-25 12:26:58 -0800

    Fine-tuning priorities for selection sources: real selection first, active element second, expanded selection: lowest

--- content.js
+++ content.js
@@ -1,21 +1,29 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     function getClickedWord() {
-        if (document.activeElement != null) {
-            var text = document.activeElement.text;
-            if (text.length > 0 && text.length <= 140) {
-                return text;
-            }
-        }
         var s = window.getSelection();
-        if (!s.isCollapsed) {
-            return s.toString();
+
+        // real selection has highest priority
+        var sText = s.toString().trim();
+        console.log("sText: " + sText);
+        if (sText.length > 0) {
+            return sText;
         }
+
+        // active elements with text are next in priority. Example: links
+        var aText = document.activeElement != null ? document.activeElement.text.trim() : "";
+        console.log("aText: " + aText);
+        if (aText.length > 0) {
+            return aText;
+        }
+
+        // lowest in priority: try to find out if we have a word clicked
         s.modify("move", "forward", "character");
         s.modify("move", "backward", "word");
         s.modify("extend", "forward", "word");
-        var res = s.toString();
+        var esText = s.toString();
+        console.log("asText: " + esText);
         s.modify("move", "forward", "character"); //clear selection
-        return res;
+        return esText;
     }
 
     if (request == "getLastClickedText") {
